998e2f8002cf3528dad3cbf6cbde28953c02120c
Manish Kumar <manish123hello@gmail.com>
2024-02-23 23:49:10 +0530

    fourth commit

--- manager/forms.py
+++ manager/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 
 class SignUpForm(UserCreationForm):
@@ -32,3 +33,16 @@ class SignUpForm(UserCreationForm):
 
 
 
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"First Name", "class": "form-control"},))
+    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name", "class": "form-contronl"},  ))
+    email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Email", "class": "form-contronl"},  ))
+    phone = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Phone", "class": "form-contronl"},  ))
+    address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Address", "class": "form-control"},  ))
+    city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"City", "class": "form-control"},  ))
+    state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"State", "class": "form-control"},  ))
+    zipcode = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Zip Code", "class": "form-control"},  ))
+
+    class Meta:
+        model = Record
+        exclude = ("user",)
--- manager/urls.py
+++ manager/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     #path('login/', views.login_user, name='login_user'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+    path('record/<int:pk>', views.customer_record, name = 'record'),
+    path('delete/<int:pk>', views.delete_record, name = 'delete'),
+    path('update/<int:pk>', views.update_record, name = 'update'),
+    path('add_record/', views.add_record, name='add'),
 ]
